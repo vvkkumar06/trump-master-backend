6a8c6661edeea0f446b5df8e073c76ffb9c86021
Vivek <vvkkumar06@gmail.com>
2023-03-08 11:22:59 +0530

    removed logic from backend

--- src/controllers/cricket-controller.js
+++ src/controllers/cricket-controller.js
@@ -6,25 +6,15 @@ const router = require('express').Router();
 const _ = require('lodash');
 const db = require('./../data/db');
 const playersData = require('./../assets/cricket/players/stats/cricket-players');
-const { transformToCardData }  = require('./../helpers/transform');
 
 
 router.get('/collection', (req, res) =>{ 
-  res.json(transformToCardData(db, playersData))
+  res.json(db)
 });
 
-router.patch('/collection/add', (req, res) =>{ 
-  let idx = db.backupCards.findIndex(i => i == req.body.id);
-   db.backupCards.splice(idx,1);  
-   db.playingCards[req.body.vacantPlayerId] = req.body.id;
-   res.json({success: true})
-})
 
-router.patch('/collection/remove', (req, res) =>{ 
-  db.playingCards[req.body.vacantPlayerId] = undefined;
-  db.backupCards.push(req.body.id)   
-  res.json({success: true})
-})
-
+router.get('/stats', (req, res) =>{ 
+  res.json(playersData)
+});
 
 module.exports = router;
--- src/data/db.js
+++ src/data/db.js
@@ -1,5 +1,33 @@
 module.exports = {
-  backupCards: [12,11,12,133,334,123,34,66,34],
+  backupCards: {
+    1: {
+      count: 5,
+    },
+    5: {
+      count: 1
+    },
+    7: {
+      count: 2
+    },
+    112: {
+      count: 2
+    },
+    115: {
+      count: 1
+    },
+    199: {
+      count: 1
+    },
+    203: {
+      count: 4
+    },
+    15: {
+      count: 1
+    },
+    4: {
+      count: 2
+    }
+  },
   playingCards: {
     card1: 123,
     card2: 154
